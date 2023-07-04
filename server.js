fd33f3e6ce4e21256163263bf9254d48cac3340f
barakaganai <barakaganai01@gmail.com>
2023-07-04 06:04:05 +0300

    implement storage for file uploads

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import multer from "multer";
 import authRoute from "./routes/auth.js";
 import userRoute from "./routes/users.js";
 import shareRoute from "./routes/shares.js";
 
 const app = express();
 dotenv.config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 //mongodb connection
 const connect = async () => {
@@ -19,10 +24,26 @@ const connect = async () => {
   }
 };
 
-// middleware
-app.use(cors());
-app.use(express.json());
+//set up storage for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
+//create uploads middleware
+const upload = multer({ storage: storage });
+app.post("api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+//API endpoint for image upload
+app.use(upload.single("image"));
+
+//middleware
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/shares", shareRoute);
