c1dc3dbb984948e5fab41e03391b46ab6240a86a
barakaganai <barakaganai01@gmail.com>
2023-07-04 05:46:46 +0300

    create Share.js in model

---
+++ models/Share.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const shareSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Share", shareSchema);
